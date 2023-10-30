f741ec88e1314a493d1ccd291d4f716d46d9505f
Stijn Hering <stijn@hering.nl>
2023-10-30 15:40:43 +0000

    updated KNMI asset

--- src/dsp-dagster/dsp_dagster/__init__.py
+++ src/dsp-dagster/dsp_dagster/__init__.py
@@ -5,9 +5,10 @@ from dagster import (
     FilesystemIOManager,  # Update the imports at the top of the file to also include this
 )
 from dagster_duckdb_polars import DuckDBPolarsIOManager
-
-
 from dsp_dagster import assets
+
+
+database_name = os.getenv("DATABASE_NAME")
 
 
 data_assets = load_assets_from_package_module(
@@ -15,30 +16,17 @@ data_assets = load_assets_from_package_module(
     group_name="data_extraction",
 )
 
-# Load Data
-# all_assets = load_assets_from_modules([load])
-
-# Define a job that will materialize the assets
-# extract_data = define_asset_job("extract_data", selection=AssetSelection.all())
-
-# # Addition: a ScheduleDefinition the job it should run and a cron schedule of how frequently to run it
-# hackernews_schedule = ScheduleDefinition(
-#     job=hackernews_job,
-#     cron_schedule="0 * * * *",  # every hour
-# )
-
 local_io_manager = FilesystemIOManager(
-    base_dir="data",  # Path is built relative to where `dagster dev` is run
+    base_dir="data/file_system_IO",  # Path is built relative to where `dagster dev` is run
 )
 
 # Insert this section anywhere above your `defs = Definitions(...)`
-database_io_manager = DuckDBPolarsIOManager(database="DSP_Project.db")
+database_io_manager = DuckDBPolarsIOManager(database=database_name)
 
 
 # Update your Definitions
 defs = Definitions(
     assets=data_assets,
-    # schedules=[hackernews_schedule],
     resources={
         "local_io_manager": local_io_manager,
         "database_io_manager": database_io_manager,  # Define the I/O manager here
--- src/dsp-dagster/dsp_dagster/assets/knmi_data.py
+++ src/dsp-dagster/dsp_dagster/assets/knmi_data.py
@@ -1,17 +1,86 @@
-from dagster import (
-    asset,
-)
+import os
+from dagster import asset, Config, get_dagster_logger
 import polars as pl
-
-
 import requests
 
+from pydantic import Field
 
-url = "https://www.daggegevens.knmi.nl/klimatologie/uurgegevens?start=2023040100&end=2023100100&stns=240&vars=all&fmt=json"
 
-payload = {}
-headers = {}
+class KNMIAssetConfig(Config):
+    base_url: str = os.getenv("KNMI_BASE_URL")
+    start: int = Field(
+        title="start",
+        description="param field `start` that is the same as the complete start-date in the format: YYYYMMDDHH",
+    )
+    end: int = Field(
+        title="end",
+        description="param field `end` that is the same as the complete end-date in the format: YYYYMMDDHH",
+    )
+    stns: int = Field(
+        title="stns",
+        description="param field `stns` that represents a weather station as integer, defaults to 240 (schiphol). A list of weather stations can be found on https://www.knmi.nl/kennis-en-datacentrum/achtergrond/data-ophalen-vanuit-een-script",
+        default=240,
+    )
+    vars: str = Field(
+        title="vars",
+        description="A list of variables that will be retrieved, separated with ':'. The list can be found on https://www.knmi.nl/kennis-en-datacentrum/achtergrond/data-ophalen-vanuit-een-script",
+    )
+    fmt: str = Field(
+        title="fmt",
+        description="Output format, this can be csv (default), json and xml",
+        default="json",
+    )
 
-response = requests.request("GET", url, headers=headers, data=payload)
+    # parmas: dict = { "start": start, "end": end}
 
-print(response.text)
+
+@asset(
+    name="knmi_uurgegevens",
+    io_manager_key="database_io_manager",  # Addition: `io_manager_key` specified
+)
+def get_knmi_uurgegevens(config: KNMIAssetConfig) -> pl.DataFrame:
+    """
+    Based from these KNMI API docs: https://www.knmi.nl/kennis-en-datacentrum/achtergrond/data-ophalen-vanuit-een-script
+
+    `start`: (verplicht)
+        De volledige datum (start) in het format YYYYMMDDHH.
+
+    `end`: (verplicht)
+        De volledige datum-tijd (end) in het format YYYYMMDDHH.
+        Het eerste en laatste uur in het HH-gedeelte bepalen de uren die voor elke dag geleverd worden (1-24), in overeeenkomst met het aan te geven dagdeel in de interactieve selectie. Dus start=2006060606, end=2008080808 resulteren voor elke dag steeds in het 6e, 7e en 8e uur, terwijl start=2006060622, end=2008080806 bv. de nachtelijke uren 22, 23 .... 5, 6 geeft.
+
+    `vars`:
+        Lijst van gewenste variabelen in willekeurige volgorde, aangeduid met hun acroniemen (zoals op de selectiepagina) gescheiden door ':', bijvoorbeeld 'DD:FH:FF:FX'.
+        De voorgedefinieerde sets van variabelen zijn hier:
+            WIND = DD:FH:FF:FX     Wind
+            TEMP = T:T10N:TD       Temperatuur
+            SUNR = SQ:Q            Zonneschijnduur en globale straling
+            PRCP = DR:RH           Neerslag en potentiële verdamping
+            VICL = VV:N:U          Zicht, bewolking en relatieve vochtigheid
+            WEER = M:R:S:O:Y:WW    Weerverschijnselen, weertypen
+            ALL alle variabelen
+    `stns`:
+        Lijst van gewenste stations (nummers) in willekeurige volgorde, gescheiden door ':'.
+        Geen default waarde; Stations móeten zijn gespecificeerd.
+        ALL staat voor álle stations.
+
+    `fmt`:
+        Standaard is de output van het script in CSV-formaat, met fmt=xml is de output in XML-formaat, met fmt=json in JSON-formaat.
+
+    :return pl.DataFrame: DataFrame with KNMI data that will be stored in DuckDB database
+    """
+    logger = get_dagster_logger()
+
+    start = config.start
+    end = config.end
+
+    params = {"start": start, "end": end}
+    response = requests.request("GET", config.base_url, params=params, timeout=60)
+
+    if response.status_code == 200:
+        data = response.json()
+        df = pl.from_dicts(data)
+        return df
+
+    else:
+        logger.error(f"Received non-200 status code [{response.status_code}]")
