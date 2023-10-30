dc46cd46008cec7a526e953166e20bca201e2904
Stijn Hering <stijn@hering.nl>
2023-10-30 16:10:28 +0000

    update

--- src/dsp-dagster/dsp_dagster/assets/knmi_data.py
+++ src/dsp-dagster/dsp_dagster/assets/knmi_data.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import os
+from typing import Optional
 from dagster import asset, Config, get_dagster_logger
 import polars as pl
 import requests
@@ -6,32 +8,38 @@ import requests
 from pydantic import Field
 
 
+def datetime_now() -> int:
+    return int(datetime.now().strftime("%Y%m%d%H"))
+
+
 class KNMIAssetConfig(Config):
     base_url: str = os.getenv("KNMI_BASE_URL")
     start: int = Field(
         title="start",
         description="param field `start` that is the same as the complete start-date in the format: YYYYMMDDHH",
+        examples=[2023010100, 2022010100],
+        default=2023010100,
     )
     end: int = Field(
         title="end",
         description="param field `end` that is the same as the complete end-date in the format: YYYYMMDDHH",
+        default_factory=datetime_now,
     )
-    stns: int = Field(
+    stns: Optional[int] = Field(
         title="stns",
         description="param field `stns` that represents a weather station as integer, defaults to 240 (schiphol). A list of weather stations can be found on https://www.knmi.nl/kennis-en-datacentrum/achtergrond/data-ophalen-vanuit-een-script",
         default=240,
     )
-    vars: str = Field(
+    vars: Optional[str] = Field(
         title="vars",
         description="A list of variables that will be retrieved, separated with ':'. The list can be found on https://www.knmi.nl/kennis-en-datacentrum/achtergrond/data-ophalen-vanuit-een-script",
+        default="ALL",
     )
-    fmt: str = Field(
+    fmt: Optional[str] = Field(
         title="fmt",
         description="Output format, this can be csv (default), json and xml",
         default="json",
     )
-
-    # parmas: dict = { "start": start, "end": end}
 
 
 @asset(
@@ -71,10 +79,13 @@ def get_knmi_uurgegevens(config: KNMIAssetConfig) -> pl.DataFrame:
     """
     logger = get_dagster_logger()
 
-    start = config.start
-    end = config.end
-
-    params = {"start": start, "end": end}
+    params = {
+        "start": config.start,
+        "end": config.end,
+        "vars": config.vars,
+        "stns": config.stns,
+        "fmt": config.fmt,
+    }
     response = requests.request("GET", config.base_url, params=params, timeout=60)
 
     if response.status_code == 200:
