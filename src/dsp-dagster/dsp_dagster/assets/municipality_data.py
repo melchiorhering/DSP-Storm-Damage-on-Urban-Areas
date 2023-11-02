4956a14efc92a6e0c0f5b2a717d74f778828ce4f
Stijn Hering <stijn@hering.nl>
2023-11-02 09:26:18 +0000

    updated trees code

--- src/dsp-dagster/dsp_dagster/assets/municipality_data.py
+++ src/dsp-dagster/dsp_dagster/assets/municipality_data.py
@@ -50,7 +50,7 @@ async def fetch_data(
     params["page"] = current_page
     response = await session.get(complete_endpoint, params=params, timeout=60)
     if response.status_code == 200:
-        logger.info(f"Page --> {current_page} | {total_pages}")
+        logger.info(f"[PAGE] {current_page} | {total_pages} [RETRIEVED]")
 
         data = response.json()
         return data["_embedded"]["stamgegevens"]
@@ -132,83 +132,3 @@ async def get_gemeente_ams_tree_data(
     )
 
     return df
-
-
-# OLD
-# @asset(
-#     name="gemeente_ams_tree_data",
-#     io_manager_key="database_io_manager",  # Addition: `io_manager_key` specified
-# )
-# def get_gemeente_ams_tree_data(
-#     context: AssetExecutionContext, config: GA_Bomen
-# ) -> pl.DataFrame:
-#     logger = get_dagster_logger()
-
-#     complete_endpoint = config.base_url + config.bomen_endpoint
-
-#     params = {
-#         "_format": config.fmt,
-#         "_count": config.count,
-#         "_pageSize": config.pageSize,
-#     }
-
-#     logger.info(f"Using {params}")
-#     response = requests.request("GET", complete_endpoint, params=params, timeout=60)
-
-#     if response.status_code == 200:
-#         logger.info(response.headers)
-#         current_page = int(response.headers["x-pagination-page"])
-#         total_count = int(response.headers["x-total-count"])
-#         total_pages = int(response.headers["x-pagination-count"])
-
-#         logger.info(f"Total number of data (trees): {total_count}")
-#         logger.info(
-#             f"Using _pageSize={params['_pageSize']} will result in {total_pages} pages"
-#         )
-
-#         data = response.json()
-#         final_data = data["_embedded"]["stamgegevens"]
-
-#         while current_page <= total_pages:
-#             logger.info(f"{current_page} | {total_pages} pages retrieved")
-#             current_page += 1
-#             params["page"] = current_page
-#             response = requests.request(
-#                 "GET", complete_endpoint, params=params, timeout=60
-#             )
-#             if response.status_code == 200:
-#                 data = response.json()
-#                 final_data += data["_embedded"]["stamgegevens"]
-#             else:
-#                 raise Failure(
-#                     description=f"Received non-200 status code [{response.status_code}]",
-#                     metadata={
-#                         "api_url": MetadataValue.url(complete_endpoint),
-#                         "total-pages": MetadataValue.text(total_pages),
-#                         "total-count": MetadataValue.text(total_count),
-#                         "errored_page": MetadataValue.int(current_page),
-#                         "params": MetadataValue.json(params),
-#                     },
-#                 )
-#     else:
-#         raise Failure(
-#             description=f"Received non-200 status code [{response.status_code}]",
-#             metadata={
-#                 "api_url": MetadataValue.url(complete_endpoint),
-#                 "total-pages": MetadataValue.text(total_pages),
-#                 "total-count": MetadataValue.text(total_count),
-#                 "errored_page": MetadataValue.int(current_page),
-#                 "params": MetadataValue.json(params),
-#             },
-#         )
-
-#     df = pl.from_dicts(final_data)
-
-#     context.add_output_metadata(
-#         metadata={
-#             "num_records": len(df),  # Metadata can be any key-value pair
-#             "preview": MetadataValue.md(df.head().to_pandas().to_markdown()),
-#             # The `MetadataValue` class has useful static methods to build Metadata
-#         }
-#     )
-#     return df
